03fc33cdd919a0f7bf566cbe7fe8d84b4ef13997
emmeiwhite <emmeiwhite@gmail.com>
2024-11-14 12:08:33 +0530

    js-1.1 ReferenceError: cart is not defined

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // var, let, const
+
 function abc() {
   var cart = []
 }
 
-console.log(cart)
+// console.log(cart)
+// UnCaught ReferenceError: cart is not defined at line 6 (cart is not in the Global scope)
+
+function abbc() {
+  console.log(cart)
+  cart = 'I am a cart, woohooo hoo hoo'
+}
+
+abbc()
+// console.log(cart)
