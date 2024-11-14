c163b47ebc0b66ad51f5955462ee7a3fd6842b1f
emmeiwhite <emmeiwhite@gmail.com>
2024-11-14 12:12:49 +0530

    js-1.2 Hoisting with var variables and regular functions!

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ function abc() {
 // UnCaught ReferenceError: cart is not defined at line 6 (cart is not in the Global scope)
 
 function abbc() {
-  console.log(cart)
-  cart = 'I am a cart, woohooo hoo hoo'
+  console.log(cart) // undefined; Hoisting in place!
+  var cart = 'I am a cart, woohooo hoo hoo' // In old days this without declared variable used to go to the global scope, but for some reason now: It is spitting error: ReferenceError: cart is not defined
 }
 
 abbc()
 // console.log(cart)
+
+// Hoisting in Action: regular function
+
+foo() // Regular function are hoisted as a whole actual function
+function foo() {
+  console.log('hello!')
+}
