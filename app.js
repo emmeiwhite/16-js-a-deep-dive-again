49c9ee9ec5ce9a9f49d6f35a5c320e86f1b0b053
emmeiwhite <emmeiwhite@gmail.com>
2024-11-14 15:14:01 +0530

    js-2.1 this: global scope(window) , regular & fxn expression (undefined), arrow (surrounding)

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 'use strict'
 console.log(this) // Points to the window object
 
+// regular function or function expression
+
+function go() {
+  console.log('Regular Function', this) // undefined
+}
+
+go()
+
 const calcAge = function (birthYear) {
   const age = new Date().getFullYear() - birthYear
   console.log(age)
-  console.log(this) // undefined in strict mode
+  console.log('Function Expression', this) // undefined (in strict mode)
+}
+
+// arrow function: this is not available in arrow function but if used, it points to the surrounding outer space!
+
+const calcPrice = (advancePayment, currentPrice) => {
+  let amountToPay = advancePayment - currentPrice
+  console.log(this) // outer surrounding Lexical Scope is window
 }
 
 calcAge(1992)
+calcPrice(30, 19)
