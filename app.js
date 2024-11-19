1964accbcaa25e3e630201e645520973474326bd
emmeiwhite <emmeiwhite@gmail.com>
2024-11-19 20:23:44 +0530

    js-3.1 Shallow Clone only with Object.assign({},obj) or with the spread operator

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /** --- 1)  primitives vs reference types --- */
 
 // Primitive Type
+
+/* ---
 let age = 30
 let oldAge = age
 
@@ -9,6 +11,7 @@ age = age + 1
 console.log(age)
 console.log(oldAge)
 
+--- */
 // Reference  Type
 
 let bottle = {
@@ -24,3 +27,24 @@ console.log('Regular Bottle:', bottle)
 console.log('Copper Bottle', copperBottle)
 
 // In both the cases the price is updated to 40, since these are reference types
+
+/** --- 2) Shallow Clone --- */
+
+const imranBeforeMarriage = {
+  name: 'Imran Rafiq Rather',
+  married: false,
+  address: {
+    state: 'J&K',
+    country: 'India',
+    pinCode: 190015,
+    city: 'Chanapora Srinagar'
+  },
+  family: ['brother', 'sister', 'parents']
+}
+
+console.log(imranBeforeMarriage)
+
+const copyImran = Object.assign({}, imranBeforeMarriage)
+
+copyImran.family.push('wife')
+console.log(copyImran)
