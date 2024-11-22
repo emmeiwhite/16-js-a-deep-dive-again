7a59c26ead1e58f42fd0aad9c4eb79f64a532cf9
emmeiwhite <emmeiwhite@gmail.com>
2024-11-22 19:35:16 +0530

    6.1 prototypes and prototypal inheritance

---
+++ 4.1-prototypal-inheritance.js
@@ -0,0 +1,4 @@
+// What is prototypal inheritance in JavaScript?
+// Prototypal inheritance is a feature in JavaScript where an object can inherit properties and methods from another object without the need to create classes. In JavaScript each and every object has a hidden link (called [[Prototype]]) to another object, known as object's prototype.
+
+/**  */
