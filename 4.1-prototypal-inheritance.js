1727d8021f9d0274f45c86f0506d79a270eabb21
emmeiwhite <emmeiwhite@gmail.com>
2024-11-25 11:40:59 +0530

    6.3 Constructor function & nature of this keyword

--- 4.1-prototypal-inheritance.js
+++ 4.1-prototypal-inheritance.js
@@ -1,4 +1,23 @@
-// Constructor Function:
+// A) What is prototypal inheritance in JavaScript?
+// Prototypal inheritance is a feature in JavaScript where an object can inherit properties and methods from another object without the need to create classes. In JavaScript each and every object has a hidden link (called [[Prototype]]) to another object, known as object's prototype.
+
+/** ---  1) Directly inheriting from another object (where another object becomes the prototype of newly created object) --- */
+
+const cat = {
+  name: 'demo cat',
+  sound: function () {
+    console.log(`${this.name} is making cute sounds`)
+  }
+}
+
+const whiteCat = Object.create(cat) // cat object acts as a prototype for whiteCat
+
+// overriding name property from the prototype | which is the cat object
+whiteCat.name = 'persian white cat'
+
+whiteCat.sound()
+
+// B) Constructor Function:
 /** ---
  * To programmatically create objects we use constructor functions.
  * The Constructor function is a regular function or a function expression. The only difference is that the constructor function is invoked with a new keyword. Arrow functions cannot be used as the constructor function.
@@ -15,3 +34,10 @@
       {} ----> prototype object
  * 4. Finally the object created in the process is returned from the constructor function to be accessible to the program using it.
  *  --- */
+
+function Person(firstName, lastName) {
+  console.log(this)
+  // this points to an empty object as soon as the function is invoked with a new keyword
+}
+
+new Person('john', 'smilga')
