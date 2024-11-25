927de8da24fbb5fb184f264d143e6956566fd619
emmeiwhite <emmeiwhite@gmail.com>
2024-11-25 11:54:34 +0530

    6.5: 4 step process when called a Constructor Function with new keyword!

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 'use strict'
 function Person(firstName, lastName) {
   console.log(this)
-  // this points to an empty object as soon as the function is invoked with a new keyword
+  /* --- 
+     Step-1: 'this' points to an empty object as soon as the function is invoked with a new keyword
+
+     this ----->{}
+
+
+  --- */
+
+  /* ---
+    Step-2: "Now that we have an empty object to begin with, & we know that this object will be returned by the constructor function, let's start to add few properties on it
+    --- */
+
+  this.FN = firstName
+  this.LN = lastName
+
+  /* ---
+      this = {
+              FN:firstName,
+              LN:lastName
+             }
+     *  --- */
+
+  /*  Step-3: The object created will be linked to a prototype  --- */
+  /*  Step-4: The created object is finally returned from the constructor function ---*/
 }
 
 new Person('john', 'smilga')
