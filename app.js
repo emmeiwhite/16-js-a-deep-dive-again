7d6d871a06ae91bd40b62ef0f818e3fc61036fc6
emmeiwhite <emmeiwhite@gmail.com>
2024-11-26 22:35:36 +0530

    6.12 Error Handing

---
+++ app.js
@@ -0,0 +1,8 @@
+/** ---
+ *  Let's do some practical stuff and learn new operators & functions in prototypal inheritance !
+ * ---*/
+
+// Q:1) How to check types in a reliable way?
+// A:1) ConstructorFunction.prototype.toString.call(true) ;// Object boolean
+
+/**  */
